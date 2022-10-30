b1d8cd5f3ddab71f8cb47973d0ab118b00c44d73
vhivestate <garcia.valeria001@gmail.com>
2022-10-30 16:29:04 -0600

    update category test

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -23,3 +23,13 @@ import {
     expect(newState.products.length).toBe(2);
     expect(initialState.products.length).toBe(0);
   });
+
+  test('UPDATE_CATEGORIES', () => {
+    let newState = reducer(initialState, {
+      type: UPDATE_CATEGORIES,
+      categories: [{}, {}]
+    });
+  
+    expect(newState.categories.length).toBe(2);
+    expect(initialState.categories.length).toBe(1);
+  });
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -10,10 +10,15 @@ import {
       case UPDATE_PRODUCTS:
         return {
           ...state,
-          products: [...action.products],
+          products: [...action.products]
+        };
+      // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+      case UPDATE_CATEGORIES:
+        return {
+          ...state,
+          categories: [...action.categories]
         };
   
-      // if it's none of these actions, do not update state at all and keep things the same!
       default:
         return state;
     }
