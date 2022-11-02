d7be66b344f4114661cb855e999e74a4e0f06871
vhivestate <garcia.valeria001@gmail.com>
2022-11-01 19:29:01 -0600

    seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -23,8 +23,8 @@ db.once('open', async () => {
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
       image: 'fern.jpg',
       category: categories[0]._id,
-      price: 2.99,
-      quantity: 500
+      price: 22.99,
+      quantity: 30
     },
     {
       name: 'Boston Fern',
@@ -32,8 +32,8 @@ db.once('open', async () => {
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
       image: 'boston.jpg',
       category: categories[0]._id,
-      price: 2.99,
-      quantity: 500
+      price: 20.00,
+      quantity: 9
     },
     {
       name: 'Spider Plant',
@@ -41,8 +41,8 @@ db.once('open', async () => {
         'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
       image: 'spider.jpg',
       category: categories[0]._id,
-      price: 1.99,
-      quantity: 500
+      price: 4.99,
+      quantity: 50
     },
     {
       name: 'Fairy Castle Cactus',
@@ -51,7 +51,7 @@ db.once('open', async () => {
         'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
       image: 'castle.jpg',
       price: 7.99,
-      quantity: 20
+      quantity: 10
     },
     {
       name: 'Paddle Cactus',
@@ -59,8 +59,8 @@ db.once('open', async () => {
       description:
         'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
       image: 'paddle.jpg',
-      price: 3.99,
-      quantity: 50
+      price: 14.99,
+      quantity: 5
     },
     {
       name: 'Bunny Ear Cactus',
@@ -68,8 +68,8 @@ db.once('open', async () => {
       description:
         'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
       image: 'bunny.jpg',
-      price: 3.99,
-      quantity: 50
+      price: 9.99,
+      quantity: 15
     },
     {
       name: 'Snake Plant',
@@ -77,7 +77,7 @@ db.once('open', async () => {
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
       image: 'snake.jpg',
-      price: 399.99,
+      price: 9.99,
       quantity: 30
     },
     {
@@ -86,8 +86,8 @@ db.once('open', async () => {
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
       image: 'pearls.jpg',
-      price: 399.99,
-      quantity: 30
+      price: 39.99,
+      quantity: 10
     },
     {
       name: 'Zebra Cactus',
@@ -95,8 +95,8 @@ db.once('open', async () => {
       description:
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
       image: 'zebra.jpg',
-      price: 199.99,
-      quantity: 30
+      price: 9.99,
+      quantity: 3
     },
     {
       name: 'Rubber Plant',
@@ -104,16 +104,16 @@ db.once('open', async () => {
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
       image: 'rubber.jpg',
-      price: 9.99,
-      quantity: 100
+      price: 19.99,
+      quantity: 19
     },
     {
       name: 'Prayer Plant',
       category: categories[4]._id,
       description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
       image: 'prayer.jpg',
-      price: 1.99,
-      quantity: 1000
+      price: 14.99,
+      quantity: 40
     },
     {
       name: 'Golden Pothos',
@@ -121,8 +121,8 @@ db.once('open', async () => {
       description:
         'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
       image: 'potho.jpg',
-      price: 2.99,
-      quantity: 1000
+      price: 24.99,
+      quantity: 100
     },
     {
       name: 'Monstera',
@@ -130,7 +130,7 @@ db.once('open', async () => {
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
       image: 'monstera.jpg',
-      price: 399.99,
+      price: 99.99,
       quantity: 30
     },
     {
@@ -139,8 +139,8 @@ db.once('open', async () => {
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
       image: 'chinese.jpg',
-      price: 399.99,
-      quantity: 30
+      price: 19.99,
+      quantity: 20
     },
     {
       name: 'Pepper Face',
@@ -149,7 +149,7 @@ db.once('open', async () => {
         'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
       image: 'pepper.jpg',
       price: 7.99,
-      quantity: 100
+      quantity: 30
     },
     {
       name: 'String of hearts',
@@ -157,8 +157,8 @@ db.once('open', async () => {
       description:
         'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
       image: 'hearts.jpg',
-      price: 7.99,
-      quantity: 100
+      price: 9.99,
+      quantity: 18
     },
     {
       name: 'Fiddle Fig',
@@ -166,8 +166,8 @@ db.once('open', async () => {
       description:
         'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
       image: 'fig.jpg',
-      price: 9.99,
-      quantity: 600
+      price: 29.99,
+      quantity: 60
     }
   ]);
 
