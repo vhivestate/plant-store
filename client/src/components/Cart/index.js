028359a3e7fe97fa9f13ae3d15f8c453c6b2befc
vhivestate <garcia.valeria001@gmail.com>
2022-11-03 20:34:20 -0600

    implement cart toggle

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -3,10 +3,30 @@ import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
 
+// import the global store and the toggle action
+import { useStoreContext } from '../../utils/GlobalState';
+import { TOGGLE_CART } from '../../utils/actions';
+
 const Cart = () => {
+    const [state, dispatch] = useStoreContext();
+
+    function toggleCart() {
+        dispatch({ type: TOGGLE_CART });
+      }
+
+      if (!state.cartOpen) {
+        return (
+          <div className="cart-closed" onClick={toggleCart}>
+            <span
+              role="img"
+              aria-label="trash">🛒</span>
+          </div>
+        );
+      }
+
   return (
     <div className="cart">
-      <div className="close">[close]</div>
+    <div className="close" onClick={toggleCart}>[close]</div>
       <h2>Shopping Cart</h2>
       <div>
           <CartItem item={{name:'Golden Pothos', image:'potho.jpg', price:24.99, purchaseQuantity:3}} />
